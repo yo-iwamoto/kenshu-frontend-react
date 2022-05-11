aabfc0ebba11210059d31fcbc1f3e5ad31c5ad63
you-5805 <you.iwamoto@prtimes.co.jp>
2022-05-11 18:37:43 +0900

    add props to Todo

--- src/App.js
+++ src/App.js
@@ -34,9 +34,9 @@ function App(props) {
       </div>
       <h2 id='list-heading'>3 tasks remaining</h2>
       <ul role='list' className='todo-list stack-large stack-exception' aria-labelledby='list-heading'>
-        <Todo />
-        <Todo />
-        <Todo />
+        <Todo name='Eat' completed={true} />
+        <Todo name='Sleep' completed={true} />
+        <Todo name='Repeat' completed={true} />
       </ul>
     </div>
   );
