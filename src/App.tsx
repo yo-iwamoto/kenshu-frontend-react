78ca7277bf8d062c43b4f35b5ae113574462ae17
you-5805 <you.iwamoto@prtimes.co.jp>
2022-05-12 12:42:32 +0900

    add filter

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,14 @@ import type { Task } from './types/Task';
 import { useState } from 'react';
 import { nanoid } from 'nanoid';
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task: Task) => !task.completed,
+  Completed: (task: Task) => task.completed,
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 type Props = {
   tasks: Task[];
 };
@@ -56,16 +64,24 @@ function App(props: Props) {
     setTasks(editedTasks);
   }
 
-  const taskList = tasks.map((task) => (
-    <Todo
-      id={task.id}
-      name={task.name}
-      completed={task.completed}
-      key={task.id}
-      toggleTaskCompleted={toggleTaskCompleted}
-      deleteTask={deleteTask}
-      editTask={editTask}
-    />
+  const [filter, setFilter] = useState('All');
+
+  const taskList = tasks
+    .filter(FILTER_MAP[filter])
+    .map((task) => (
+      <Todo
+        id={task.id}
+        name={task.name}
+        completed={task.completed}
+        key={task.id}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask={deleteTask}
+        editTask={editTask}
+      />
+    ));
+
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton key={name} name={name} isPressed={name === filter} setFilter={setFilter} />
   ));
 
   const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
@@ -76,11 +92,7 @@ function App(props: Props) {
     <div className='todoapp stack-large'>
       <h1>TodoMatic</h1>
       <Form addTask={addTask} />
-      <div className='filters btn-group stack-exception'>
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
-      </div>
+      <div className='filters btn-group stack-exception'>{filterList}</div>
       <h2 id='list-heading'>{headingText}</h2>
       {/* eslint-disable-next-line jsx-a11y/no-redundant-roles */}
       <ul role='list' className='todo-list stack-large stack-exception' aria-labelledby='list-heading'>
--- src/components/FilterButton.tsx
+++ src/components/FilterButton.tsx
@@ -1,10 +1,21 @@
 import type { FC } from 'react';
 
-const FilterButton: FC = () => {
+type Props = {
+  name: string;
+  isPressed: boolean;
+  setFilter: (name: string) => void;
+};
+
+const FilterButton: FC<Props> = (props) => {
   return (
-    <button type='button' className='btn toggle-btn' aria-pressed='true'>
+    <button
+      type='button'
+      className='btn toggle-btn'
+      aria-pressed={props.isPressed}
+      onClick={() => props.setFilter(props.name)}
+    >
       <span className='visually-hidden'>Show </span>
-      <span>all </span>
+      <span>{props.name} </span>
       <span className='visually-hidden'> tasks</span>
     </button>
   );
